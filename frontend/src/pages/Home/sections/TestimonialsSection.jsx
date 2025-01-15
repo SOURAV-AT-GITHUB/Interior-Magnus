d5096a68c166c8190bcc397d8ae4564dc7153f2b
Söùrãv <souravpramanick1997@gmail.com>
2025-01-15 16:59:46 +0530

    Author image added on testimonials

--- frontend/src/pages/Home/sections/TestimonialsSection.jsx
+++ frontend/src/pages/Home/sections/TestimonialsSection.jsx
@@ -1,8 +1,11 @@
-import testimonial1 from "/testimonial1.jfif";
-import testimonial2 from "/testimonial2.jfif";
-import testimonial3 from "/testimonial3.jfif";
-import testimonalStrip from "/testimonal-strips.svg";
-import testimonialCircle from "/testimonial-circle.svg";
+import testimonial1 from "/testimonials/testimonial1.jfif";
+import testimonial2 from "/testimonials/testimonial2.jfif";
+import testimonial3 from "/testimonials/testimonial3.jfif";
+import testimonial1author from '/testimonials/testimonial1-author.png'
+import testimonial2author from '/testimonials/testimonial2-author-test.png'
+import testimonial3author from '/testimonials/testimonial3-author.png'
+import testimonalStrip from "/testimonials/testimonal-strips.svg";
+import testimonialCircle from "/testimonials/testimonial-circle.svg";
 import { useState } from "react";
 import WestIcon from "@mui/icons-material/West";
 import EastIcon from "@mui/icons-material/East";
@@ -11,18 +14,24 @@ export default function TestimonialsSection() {
   const testimonials = [
     {
       image: testimonial1,
-      text: "Working with Interior Magnus was an absolute pleasure. They took our ideas and elevated them beyond what we could imagine. Our home now feels both luxurious and comfortable!",
-      author: "Priya Malhotra",
+      text: "Working with Interior Magnus was an absolute pleasure from start to finish. Their team not only took the time to truly understand our ideas, but they also brought a level of creativity and expertise that transformed our vision into something beyond what we could have ever imagined. Throughout the process, they provided thoughtful suggestions and elegant design solutions that made our home feel both luxurious and welcoming. Every detail was carefully considered, and their commitment to quality was evident in every aspect of the project. Thanks to interior Magnus, our space now perfectly reflects our style and feels like a place we love to call home.",
+      author: "Pradyot and Shalini",
+      authorImage:testimonial1author,
+      property : "Elte Hamz, Sec 77, Noite"
     },
     {
       image: testimonial2,
-      text: "The designers at Interior Magnus listened to every detail we wanted in our office space and brought it all together beautifully. They managed everything effortlessly. Highly recommend their services!",
-      author: "Rajesh Kumar",
+      text: "Choosing Interior Magnus was the best decision we made for our renovation. Their team was attentive, thoughtful, and truly invested in bringing our vision to life. From the initial consultation to the final touches, they provided guidance and suggestions that elevated our ideas in ways we hadn't considered. The attention to detail, craftsmanship, and quality they brought to our space left us thrilled with the outcome. Now, our home feels like an elegant, comfortable sanctuary that perfectly captures our style. We couldn't be happier with the results!",
+      author: "Shyam Singh",
+      authorImage:testimonial2author,
+      property : "CRC Suolens, Noda Extension"
     },
     {
       image: testimonial3,
-      text: "I couldn’t be happier with the transformation of our guest room and kitchen! The team created designs that are not only stunning but highly practical. They made our spaces feel fresh and modern.",
-      author: "Vaibhav Vaish",
+      text: "Interior Magnus turned our dream home into a reality. They listened to our preferences and crafted designs that perfectly reflected our tastes while introducing unique elements that made the space truly stand out. Their process was smooth and professional, with every detail carefully thought out and executed flawlessly. The team was responsive, creative, and dedicated to ensuring our satisfaction at every step. Thanks to their expertise, we now live in a space that feels luxurious, comfortable, and completely us. We highly recommend Interior Magnus!",
+      author: "Dinesh & Shruti",
+      authorImage:testimonial3author,
+      property : "ATS e Tretto"
     },
   ];
   // const [reverse, setReverse] = useState(false);
@@ -139,28 +148,35 @@ export default function TestimonialsSection() {
                   }
                   ${activeIndex === testimonials.length-1 && 'mr-1'}
                   transition-all duration-500 ease-in-out
-                  w-full min-w-[275px]  lg:min-w-[360px] lg:min-h-[500px] 
+                  w-full min-w-[275px]  lg:min-w-[360px] lg:h-auto h-fit
                   mb-4 border border-black rounded-3xl overflow-hidden shadow-buttonShadow`}
                 >
+                  <div className="relative h-2/6 w-full ">
+
                   <img
                     src={testimonial.image}
                     alt={testimonial.author}
-                    className={`h-2/4 w-full    `}
+                    className={`w-full h-full object-cover object-center`}
                   />
+                  <img src={testimonial.authorImage} 
+                  alt=""
+                  className="absolute h-full bottom-0 left-2/4 -translate-x-2/4"/>
                   <img
                     src={testimonialCircle}
                     alt="testimonialCircle"
-                    className="absolute right-2 top-[40%] z-10 max-[500px]:h-20"
+                    className="absolute right-2 -bottom-8 z-10 h-20"
                   />
+                  </div>
 
                   <div
-                    className={`h-2/4 flex flex-col justify-center bg-secondary text-white p-4 `}
+                    className={`h-4/6 flex flex-col  bg-secondary text-white p-4 pt-6`}
                   >
                     <p className="">{testimonial.text}</p>
                     <p className="flex items-center text-lg">
                       <span className="w-8 h-0 px-2  border-t border-white"></span>
                       &nbsp; {testimonial.author}
                     </p>
+                    <p className="">Property : {testimonial.property}</p>
                   </div>
                 </div>
               ))}
