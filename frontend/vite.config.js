f827f2ea067de0c9f9ebe441bc345c1019181792
Söùrãv <souravpramanick1997@gmail.com>
2025-01-17 00:28:15 +0530

    deployment issues fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // This ensures relative paths for assets and routes (important for static hosting)
   build: {
     target: 'esnext', // Ensures the build is using modern JavaScript (ES modules)
     modulePreload: true, // Ensures the use of <link rel="modulepreload"> for faster loading
