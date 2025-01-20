d8eb14dc5081de2d50ce74acf13c3f1784af5b16
Söùrãv <souravpramanick1997@gmail.com>
2025-01-20 11:27:03 +0530

    Button component and instagram svg updated

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,9 +1,9 @@
-export default function Button({ text = "Default", color = "primary",onClick=()=>{return} }) {
+export default function Button({ text = "Default", color = "primary",onClick=()=>{return},isDisabled=false }) {
   return (
-    <div className={`bg-${color} w-fit p-1 shadow-buttonShadow`} onClick={onClick}>
-      <button className="text-white border border-black p-2  text-xl">
+    <button disabled={isDisabled} className={`bg-${color} w-fit p-1 shadow-buttonShadow min-w-[100px] disabled:opacity-75 disabled:cursor-progress`} onClick={onClick}>
+      <p className="w-full text-white border border-black p-2  text-xl">
         {text}
-      </button>
-    </div>
+      </p>
+    </button>
   );
 }
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -44,8 +44,8 @@ export default function Footer() {
             >
               <img src={linkedinIcon} alt="linkedin-icon" className="h-4/6" />
             </a>
-            <a href="https://www.instagram.com/interiormagnus/" target="_blank" rel="noopener noreferrer" className="h-4/6 overflow-hidden p-1 border-[2.5px] rounded-full">
-            <img src={instagramIcon} alt="linkedin-icon" className="h-full   scale-75" /></a>
+            <a href="https://www.instagram.com/interiormagnus/" target="_blank" rel="noopener noreferrer" className="h-fit sm:h-4/6 p-[2px] sm:p-1 border-[1.6px] rounded-full">
+            <img src={instagramIcon} alt="linkedin-icon" className="max-h-[25px] sm:max-h-full  scale-75" /></a>
           </div>
         </div>
 
